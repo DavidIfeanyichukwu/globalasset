44d0fc199ea0eee9115b3afde883bd66d7eec7d8
John <jwight@wight-space.com>
2018-07-21 03:16:45 -0600

    add error checking

--- app/Utils.php
+++ app/Utils.php
@@ -7,27 +7,32 @@ use Carbon\Carbon;
 class Utils
 {
   public static function dueAtDisplayState($due_at) {
-    $carbon = new Carbon();
-    $current = $carbon->today(); // Today 
-    //$due_at = "2018-12-20";
+    if(isset($due_at)) {
+      $carbon = new Carbon();
+      $current = $carbon->today(); // Today 
+      //$due_at = "2018-12-20"; Test Date
+      
+      $green = 60; //You have time - green
+      $yellow = 30; //Time is running out - yellow
+      $red = 10; //You are out of time - red
+      $black = 0; //This stuff is bad - black
+      $print = ""; // Empty
+
+      if( $current->diffInDays($due_at) >= $green ) {
+          $print = "green";
+      }else if( $current->diffInDays($due_at) >=  $yellow ) {
+          $print = "yellow";
+      }else if( $current->diffInDays($due_at) >=  $red ) {
+          $print = "red";
+      }else if( $current->diffInDays($due_at) >=  $black ) {
+          $print = "black";
+      }
+
+      return  $print;
     
-    $green = 60; //You have time - green
-    $yellow = 30; //Time is running out - yellow
-    $red = 10; //You are out of time - red
-    $black = 0; //This stuff is bad - black
-    $print = "";
-
-    if( $current->diffInDays($due_at) >= $green ) {
-        $print = "green";
-    }else if( $current->diffInDays($due_at) >=  $yellow ) {
-        $print = "yellow";
-    }else if( $current->diffInDays($due_at) >=  $red ) {
-        $print = "red";
-    }else if( $current->diffInDays($due_at) >=  $black ) {
-        $print = "black";
+    }else {
+      throw new \Exception("Make sure due_at is setup in DataBase/Bread!");
     }
-
-    return  $print;
   }
 
 }
